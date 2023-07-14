41dfc419312fea208c9fddbf3bd555510299c4ae
takeshi.nagaoka <takeshi-nagaoka@users.noreply.github.com>
2023-07-14 14:57:14 +0900

    Update streamlit_app.py
    urlを読み込んで回答するように修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,23 @@
 import streamlit as st
+import requests
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+
+
+def fetch_data_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        st.error(f"Failed to fetch data from URL: {url}")
+        return None
 
 
 def main():
     llm = ChatOpenAI(temperature=0)
 
     st.set_page_config(
-        page_title="My Great ChatGPT",
+        page_title="ツォクトモンゴル乗馬ツアーのAIチャットボット🐴",
         page_icon="🐴"
     )
     st.header("ツォクトモンゴル乗馬ツアーのAIチャットボット🐴")
@@ -18,39 +28,37 @@ def main():
             SystemMessage(content="ご質問を入力ください。QAを元にAIが回答いたします。")
         ]
 
-    # サイドバーのタイトルを表示
-    st.sidebar.title("Options")
+    # URLからデータを取得
+    url = "https://mongol-jyouba-gakkou.com/attention"
+    data = fetch_data_from_url(url)
 
-    def init_messages():
-        clear_button = st.sidebar.button("チャット履歴を削除", key="clear")
-        if clear_button:
-            st.session_state.messages = []
+    if data:
+        # ユーザーの入力を監視
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_area(label='Message: ', key='input', height=100, value=data, readonly=True)
+            submit_button = st.form_submit_button(label='Send')
 
-    init_messages()
+            if submit_button and user_input:
+                # ユーザーが入力し、Submitボタンが押された場合に実行されるコード
+                st.session_state.messages.append(HumanMessage(content=user_input))
+                with st.spinner("お馬さんが考えています...."):
+                    response = llm(st.session_state.messages)
+                st.session_state.messages.append(AIMessage(content=response.content))
 
-    # ユーザーの入力を監視
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area(label='Message: ', key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
+        # チャット履歴の表示
+        messages = st.session_state.get('messages', [])
+        for message in messages:
+            if isinstance(message, AIMessage):
+                with st.chat_message('assistant'):
+                    st.markdown(message.content)
+            elif isinstance(message, HumanMessage):
+                with st.chat_message('user'):
+                    st.markdown(message.content)
+            else:  # isinstance(message, SystemMessage):
+                st.write(f"System message: {message.content}")
 
-        if submit_button and user_input:
-            # ユーザーが入力し、Submitボタンが押された場合に実行されるコード
-            st.session_state.messages.append(HumanMessage(content=user_input))
-            with st.spinner("お馬さんが考えています...."):
-                response = llm(st.session_state.messages)
-            st.session_state.messages.append(AIMessage(content=response.content))
-
-    # チャット履歴の表示
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message('assistant'):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message('user'):
-                st.markdown(message.content)
-        else:  # isinstance(message, SystemMessage):
-            st.write(f"System message: {message.content}")
+    else:
+        st.error("Failed to fetch data from URL.")
 
 
 if __name__ == '__main__':
