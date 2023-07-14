0521fb2d5f82cf6f5e2560cd13aaac14262f39da
takeshi.nagaoka <takeshi-nagaoka@users.noreply.github.com>
2023-07-14 15:04:53 +0900

    Update streamlit_app.py
    URLの内容を元に回答を返すように修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+import requests
+
+
+def fetch_data_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
 
 def main():
@@ -28,6 +37,10 @@ def main():
 
     init_messages()
 
+    # URLからデータを取得
+    url = "https://mongol-jyouba-gakkou.com/attention"
+    data = fetch_data_from_url(url)
+
     # ユーザーの入力を監視
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area(label='Message: ', key='input', height=100, value='')
@@ -36,9 +49,11 @@ def main():
         if submit_button and user_input:
             # ユーザーが入力し、Submitボタンが押された場合に実行されるコード
             st.session_state.messages.append(HumanMessage(content=user_input))
-            with st.spinner("お馬さんが考えています...."):
-                response = llm(st.session_state.messages)
-            st.session_state.messages.append(AIMessage(content=response.content))
+
+            # URLの内容を使用して回答を生成
+            response = generate_response(data, user_input)
+
+            st.session_state.messages.append(AIMessage(content=response))
 
     # チャット履歴の表示
     messages = st.session_state.get('messages', [])
@@ -51,6 +66,12 @@ def main():
                 st.markdown(message.content)
         else:  # isinstance(message, SystemMessage):
             st.write(f"System message: {message.content}")
+
+
+def generate_response(data, user_input):
+    # URLの内容を使用して回答を生成するロジックを実装
+    # ここでは単純にURLの内容をそのまま返す例としています
+    return data
 
 
 if __name__ == '__main__':
